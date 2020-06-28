4ef9da607f226d7991c3b9ccd4a1dded4f130636
dieter <vanderdt@ucmail.uc.edu>
2020-06-28 18:55:11 -0400

    finalizing sound localization

--- tone_generation/make_pip.py
+++ tone_generation/make_pip.py
@@ -3,8 +3,20 @@ import math
 from matplotlib import pyplot
 from scipy.signal.windows import hann
 from scipy.io.wavfile import write as wav_write
+from scipy.signal import butter, lfilter
 
 fs = 44100
+
+class HighPassFilter:
+    def __init__(self, cutoff, fs, order):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        self.b, self.a = butter(order, normal_cutoff, btype='high', analog=False)
+
+    def run(self, signal):
+        y = lfilter(self.b, self.a, signal)
+        return y
+
 
 
 def signal_ramp(n, percent):
@@ -29,8 +41,34 @@ def make_pip(duration, frequency):
     return wave
 
 
-pip = make_pip(0.25, 1000)
-silence = make_pip(0.1, 0) * 0
+def make_burst(duration):
+    pip_samples = math.ceil(fs * duration)
+    #wave = (numpy.random.random(pip_samples) * 2) - 1
+    wave = numpy.random.normal(size=pip_samples)
+    window = signal_ramp(pip_samples, 5)
+    wave = wave * window
+    return wave
 
-sound = numpy.concatenate((pip, silence))
+
+repeats = 10
+silence = make_pip(0.25, 0) * 0
+hp_filter = HighPassFilter(3000, fs, 6)
+parts = []
+for i in range(repeats):
+    burst = make_burst(0.5)
+    burst = hp_filter.run(burst)
+    parts.append(burst)
+    parts.append(silence)
+
+sound = numpy.concatenate(parts)
+
+pyplot.plot(sound)
+pyplot.show()
+
 wav_write('pip.wav', fs, sound)
+
+# pip = make_pip(0.25, 1000)
+
+#
+#sound = numpy.concatenate((pip, silence))
+
